29533379debd0c65315047fb1c5e746c031a44de
JUNG_TK\Kkitut <digitalhdmiport@gmail.com>
2026-05-29 05:55:43 +0900

    1.1.0

--- ADOFAI_yt_dlp/Info.cs
+++ ADOFAI_yt_dlp/Info.cs
@@ -2,7 +2,7 @@ namespace ADOFAI_yt_dlp;
 
 public static class Info {
     public const string Name = "ADOFAI_yt_dlp";
-    public const string Version = "1.0.0";
+    public const string Version = "1.1.0";
     public const string Author = "KkitutLab";
     public const string Github = "https://github.com/KkitutLab/ADOFAI_yt_dlp";
 }
